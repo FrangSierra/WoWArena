212d910a536a608be1800451a7f9d397c2477d1e
Francisco Garcia Sierra <francisco.garciasierra@bq.com>
2017-12-18 13:01:09 +0100

    Initial commit

---
+++ app/src/main/java/durdinstudios/wowarena/core/WowArenaApplication.kt
@@ -0,0 +1,5 @@
+package durdinstudios.wowarena.core
+
+/**
+ * Created by fragarsie on 12/18/17.
+ */
---
+++ app/src/main/java/durdinstudios/wowarena/data/networking/PvPLeaderboardsService.kt
@@ -0,0 +1,5 @@
+package durdinstudios.wowarena.data.networking
+
+/**
+ * Created by fragarsie on 12/18/17.
+ */
